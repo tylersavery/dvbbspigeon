41e52d541366ae6094b56c6c6b2a8a08971d5dc0
tylersavery <tyler@dinosaur-factory.com>
2012-01-25 17:59:17 -0500

    facebook size

--- private/views/mobile_view.php
+++ private/views/mobile_view.php
@@ -126,3 +126,24 @@
     </div>
 </div>
 
+
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+
+<script>
+
+FB.init({
+    appId : '284190808302685',
+    status : true, // check login status
+    cookie : true, // enable cookies to allow the server to access the session
+    xfbml : true // parse XFBML
+});
+
+window.fbAsyncInit = function() {
+    FB.Canvas.setSize();
+}
+
+// Do things that will sometimes call sizeChangeCallback()
+
+function sizeChangeCallback() {
+    FB.Canvas.setSize();
+}
