8f8d06ab226a10124e90a21e7613ccc3143bc4d9
tylersavery <tyler@dinosaur-factory.com>
2012-01-25 18:12:58 -0500

    facebook iframe

---
+++ public/facebook/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+    <title>D V B B S</title>
+<style type="text/css">
+*{
+    margin:0;
+    padding:0;
+}
+body{
+    width:520px;
+    height:300px;
+    overflow:hidden;
+}
+</style>
+</head>
+<body>
+<iframe width="520" height="350" src="http://dvbbs.theyoungastronauts.com?f=1"></iframe>
+</body>
+</html>
