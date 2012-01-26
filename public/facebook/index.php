bda262130868c74cba3b62302d4919717bb83a30
tylersavery <tyler@dinosaur-factory.com>
2012-01-25 20:43:47 -0500

    fb social

--- public/facebook/index.php
+++ public/facebook/index.php
@@ -16,9 +16,102 @@ body{
 iframe{
     border:none;
 }
+
+
+
+
+.social{
+    width:230px;
+    height:18px;
+    position:relative;
+    float:left;
+    margin:3px auto;
+    margin-left:150px;
+}
+
+.social .social_item{
+    position:relative;
+    width:34px;
+    height:17px;
+    float:left;
+    margin-right: 4px;
+    text-align:center;
+}
+
+.social .social_item a{
+    background-image:url(/images/social.png);
+    text-indent:-9999px;
+    display:block;
+    width:34px;
+    height:17px;
+    background-repeat:no-repeat;
+}
+
+#social_facebook{
+    background-position: -9px -26px;
+    width:33px;
+}
+
+#social_facebook:hover{
+    background-position: -9px -2px;
+}
+
+#social_twitter{
+    background-position: -49px -26px;
+}
+
+#social_twitter:hover{
+    background-position: -49px -2px;
+}
+
+#social_youtube{
+    background-position: -89px -26px;
+}
+
+#social_youtube:hover{
+    background-position: -89px -2px;
+}
+
+#social_soundcloud{
+    background-position: -130px -26px;
+    width: 33px;
+    
+}
+
+#social_soundcloud:hover{
+    background-position: -130px -2px;
+}
+
+#social_tumblr{
+    background-position: -169px -26px;
+}
+
+#social_tumblr:hover{
+    background-position: -169px -2px;
+}
+
+#social_itunes{
+    background-position: -209px -26px;
+}
+
+#social_itunes:hover{
+    background-position: -209px -2px;
+}
+
+
 </style>
 </head>
 <body>
 <iframe width="520" height="380" src="http://dvbbs.theyoungastronauts.com?f=1"></iframe>
+<div style="clear:both;"></div>
+ <div class="social">
+    <div class="social_item"><a href="http://twitter.com/dvbbs" id="social_twitter" target="_blank">twitter</a></div>
+    <div class="social_item"><a href="http://youtube.com/dvbbs" id="social_youtube" target="_blank">youtube</a></div>
+    <div class="social_item"><a href="http://soundcloud.com/DVBBS" id="social_soundcloud" target="_blank">soundcloud</a></div>
+     <div class="social_item"><a href="#" id="social_tumblr" target="_blank">tumblr</a></div>
+    <div class="social_item last"><a href="#" id="social_itunes" target="_blank">itunes</a></div>
+</div>
+
+
 </body>
 </html>
