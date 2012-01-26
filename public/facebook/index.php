3f7287360caa69da6988be31de926ec19429f8d3
tylersavery <tyler@dinosaur-factory.com>
2012-01-26 11:00:17 -0500

    social icons for facebook app

--- public/facebook/index.php
+++ public/facebook/index.php
@@ -19,93 +19,82 @@ iframe{
 
 
 .social{
-    width:186px;
-    height:18px;
-    position:absolute;
-    float:left;
+    width:516px;
+    height:30px;
+    position:relative;
     margin:0px auto;
     right:0px;
-    text-align:right;
+    padding-left:4px;
+    background-color:#231f20;
+    margin-top:-10px;
+
 }
 
 .social .social_item{
     position:relative;
-    width:34px;
-    height:17px;
+    height:30px;
     float:left;
-    margin-right: 4px;
-    text-align:center;
-}
-
-.social .social_item.last{
-    margin-right:0px;
 }
 
 .social .social_item a{
-    background-image:url(/images/social.png);
+    background-image:url(/images/dvbbs_socialicons_facebook.gif);
     text-indent:-9999px;
     display:block;
-    width:34px;
-    height:17px;
+    width:104px;
+    height:30px;
     background-repeat:no-repeat;
 }
 
-#social_facebook{
-    background-position: -9px -26px;
-    width:33px;
-}
-
-#social_facebook:hover{
-    background-position: -9px -2px;
-}
 
 #social_twitter{
-    background-position: -49px -26px;
+    background-position: 0px -30px;
 }
 
 #social_twitter:hover{
-    background-position: -49px -2px;
+    background-position: 0px 0px;
 }
 
 #social_youtube{
-    background-position: -89px -26px;
+    background-position: -104px -30px;
 }
 
 #social_youtube:hover{
-    background-position: -89px -2px;
+    background-position: -104px 0px;
 }
 
 #social_soundcloud{
-    background-position: -130px -26px;
-    width: 33px;
-    
+    background-position: -208px -30px;
+    width:100px;
 }
 
 #social_soundcloud:hover{
-    background-position: -130px -2px;
+    background-position: -208px 0px;
 }
 
 #social_tumblr{
-    background-position: -169px -26px;
+    background-position: -312px -30px;
+    width:100px;
 }
 
 #social_tumblr:hover{
-    background-position: -169px -2px;
+    background-position: -312px 0px;
 }
 
 #social_itunes{
-    background-position: -209px -26px;
+    background-position: -416px -30px;
 }
 
 #social_itunes:hover{
-    background-position: -209px -2px;
+    background-position: -416px -0px;
     margin-right:0px;
 }
 
+
+
 #fbplugin{
-    background-color:#000;
+    background-color:#231f20;
     position:relative;
-    margin-top:26px;
+    margin-top:0px;
     padding-left:2px;
 }
 
@@ -114,12 +103,12 @@ iframe{
 </head>
 <body>
 <iframe width="520" height="380" src="http://dvbbs.theyoungastronauts.com?f=1"></iframe>
-<div style="clear:both;"></div>
  <div class="social">
     <div class="social_item"><a href="http://twitter.com/dvbbs" id="social_twitter" target="_blank">twitter</a></div>
+    
     <div class="social_item"><a href="http://youtube.com/dvbbs" id="social_youtube" target="_blank">youtube</a></div>
     <div class="social_item"><a href="http://soundcloud.com/DVBBS" id="social_soundcloud" target="_blank">soundcloud</a></div>
-     <div class="social_item"><a href="#" id="social_tumblr" target="_blank">tumblr</a></div>
+    <div class="social_item"><a href="#" id="social_tumblr" target="_blank">tumblr</a></div>
     <div class="social_item last"><a href="#" id="social_itunes" target="_blank">itunes</a></div>
 </div>
  <div id="fbplugin">
